b53e7eced71a75861b34e189eb40420c0a5be784
Kaushal Chamarthy <kaushal7343@gmail.com>
2021-10-24 22:15:52 -0500

    first commit

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="App-background"/>
+      <header className="Search-Bar">
+        <form action="/" method="get">
+            <input  
+                type="text"
+                id="header-search"
+                placeholder="Search stocks"
+                name="s" 
+            />
+            <button type="submit">Search</button>
+        </form>
+        
+        <button class="tablink" onclick="openContent('Top_Stocks', this, '#ed9d09')" id ="defaultOpen"> Top Stocks </button>
+        <button class="tablink" onclick="openContent('My_Stocks', this, '#ed9d09')"> My Stocks </button>
+        <button class="tablink" onclick="openContent('How_it_Works', this, '#ed9d09')"> How it Works </button>
+        <button class="tablink" onclick="openContent('Get_Started', this, '#ed9d09')"> Get started </button>
+        <button class="tablink" onclick="openContent('Log_In', this, '#ed9d09')"> Log in </button>
+
+        
       </header>
     </div>
-  );
+  )
 }
+
 
 export default App;
